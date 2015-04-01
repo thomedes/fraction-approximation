392a819d66b3d8ce3a87fdfeeae89773f4ca9199
Toni Homedes <toni@homedes.net>
2015-04-01 22:53:25 +0200

    indent

--- fractions.c
+++ fractions.c
@@ -6,27 +6,29 @@
 #define fraction(a) ((float) a.n / a.d)
 
 struct fraction_precision {
-    unsigned n, d;
-    float precision;
+    unsigned        n,
+                    d;
+    float           precision;
 };
 
-struct fraction_precision closest_fraction(double x, int steps, 
-                                           int (*callback)(struct fraction_precision))
+struct fraction_precision closest_fraction(double x, int steps,
+                                           int (*callback) (struct
+                                                            fraction_precision))
 {
     struct fraction_precision loop = { 0, 1, 0 }, best = loop, optimum = loop;
 
     if (x < 0) {
         x = -x;
     }
-    
+
     while (--steps) {
-        double y = fraction(loop);
-        
+        double          y = fraction(loop);
+
         loop.precision = x == y ? 9999 : -log10((y > x ? y / x : x / y) - 1);
-        
+
         if (loop.precision > best.precision) {
             if (value(loop) > value(optimum)) {
-                steps = (*callback)(optimum = loop);
+                steps = (*callback) (optimum = loop);
             }
             best = loop;
         }
@@ -34,18 +36,18 @@ struct fraction_precision closest_fraction(double x, int steps,
     }
     return best;
 }
+
 #undef value
 #undef fraction
 
 int evaluate(struct fraction_precision f)
 {
-    printf("%10d / %-10d => %.10f (%4.1f)\n", 
+    printf("%10d / %-10d => %.10f (%4.1f)\n",
            f.n, f.d, (float) f.n / f.d, f.precision);
-    return 1000; /* How many more steps to try */
+    return 1000;                           /* How many more steps to try */
 }
 
-
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
     struct fraction_precision result;
 
@@ -53,6 +55,6 @@ int main(int argc, char *argv[])
     result = closest_fraction(3.14159265359, 1000, &evaluate);
     printf("Best result:\n");
     evaluate(result);
-    
+
     return EXIT_SUCCESS;
 }
